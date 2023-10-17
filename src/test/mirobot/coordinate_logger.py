ca4bd2d62df89c5ddd728feaeb5304b921923864
MichaelTA <michaelta2000@protonmail.com>
2023-10-17 17:02:48 +0100

    Add a coordinate logger to continuously poll the Mirobot

---
+++ src/test/mirobot/coordinate_logger.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+class CoordinateLogger:
+    def __init__(self):
+        start_time_str = datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%SZ')
+        self.filename = f'mirobot_coordinate_log_{start_time_str}.csv'
+        self.coordinate_list = []
+
+        with open(self.filename, 'w') as f:
+            f.write('x,y,z,roll,pitch,yaw,j1,j2,j3,j4,j5,j6\n')
+
+    def log_coordinates(self, arm):
+        status = arm.get_status()
+        coordinate_str = (f'{status.cartesian.x},{status.cartesian.y},{status.cartesian.z},'
+                          f'{status.cartesian.roll},{status.cartesian.pitch},{status.cartesian.yaw},'
+                          f'{status.angle.joint1},{status.angle.joint2},{status.angle.joint3},'
+                          f'{status.angle.joint4},{status.angle.joint5},{status.angle.joint6}\n')
+        self.coordinate_list.append(coordinate_str)
+
+        if len(self.coordinate_list) >= 50:
+            with open(self.filename, 'a') as f:
+                [f.write(line) for line in self.coordinate_list]
+            self.coordinate_list.clear()
+
--- src/test/mirobot/mirobot_test2.py
+++ src/test/mirobot/mirobot_test2.py
@@ -2,11 +2,11 @@ import random
 from threading import Thread
 import time
 import cv2
-from wlkata_mirobot import WlkataMirobot, WlkataMirobotTool
+from wlkata_mirobot import WlkataMirobot
+
+from coordinate_logger import CoordinateLogger
 
 
-x, y, z = 100, 100, 100
-a, b, c = 0, 0, 0
 do_open, do_close = False, False
 joints = {1: 45, 2: -30, 3: 50, 4: 0, 5: -25, 6: -45}
 
@@ -29,83 +29,17 @@ def camera_thread():
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
-        # elif key == ord('o'):
-        #     do_open = True
-        #     time.sleep(0.2)
-        # elif key == ord('c'):
-        #     do_close = True
-        #     time.sleep(0.2)
-        # elif key == ord('u'):
-        #     x -= 10
-        #     print(f'Decreasing x to {x}')
-        #     time.sleep(0.2)
-        # elif key == ord('v'):
-        #     y -= 10
-        #     print(f'Decreasing y to {y}')
-        #     time.sleep(0.2)
-        # elif key == ord('w'):
-        #     z -= 10
-        #     print(f'Decreasing z to {z}')
-        #     time.sleep(0.2)
-        # elif key == ord('x'):
-        #     x += 10
-        #     print(f'Increasing x to {x}')
-        #     time.sleep(0.2)
-        # elif key == ord('y'):
-        #     y += 10
-        #     print(f'Increasing y to {y}')
-        #     time.sleep(0.2)
-        # elif key == ord('z'):
-        #     z += 10
-        #     print(f'Increasing z to {z}')
-        #     time.sleep(0.2)
-
-
-def parse_command():
-    global x, y, z, a, b, c, do_open, do_close
-
-    command = input()
-    print(command)
-
-    command = command.split(' ')
-    if command[0] == 'x':
-        x = int(command[1])
-    if command[0] == 'y':
-        y = int(command[1])
-    if command[0] == 'z':
-        z = int(command[1])
-    if command[0] == 'a':
-        a = int(command[1])
-    if command[0] == 'b':
-        b = int(command[1])
-    if command[0] == 'c':
-        c = int(command[1])
-    if command[0] == 'j1':
-        joints[1] = int(command[1])
-    if command[0] == 'j2':
-        joints[2] = int(command[1])
-    if command[0] == 'j3':
-        joints[3] = int(command[1])
-    if command[0] == 'j4':
-        joints[4] = int(command[1])
-    if command[0] == 'j5':
-        joints[5] = int(command[1])
-    if command[0] == 'j6':
-        joints[6] = int(command[1])
-    if command[0] == 'open':
-        do_open = True
-    if command[0] == 'close':
-        do_close = True
 
 
 def arm_thread():
-    global x, y, z, a, b, c, do_open, do_close
-    global joints
+    global do_open, do_close, joints
 
     print('Arm Starting')
 
-    arm = WlkataMirobot(portname='COM3', debug=True)
+    arm = WlkataMirobot(portname='COM3', default_speed=20)
     arm.home()
+
+    logger = CoordinateLogger()
 
     previous_time = time.time()
 
@@ -122,7 +56,9 @@ def arm_thread():
             print(f'Setting joints to: \n1 {joints[1]}\n2 {joints[2]}\n3 {joints[3]}\n4 {joints[4]}\n5 {joints[5]}\n'
                   f'6 {joints[6]}')
 
-        arm.set_joint_angle(joints, speed=200)
+        arm.set_joint_angle(joints, speed=20)
+
+        logger.log_coordinates(arm)
 
         if do_open:
             arm.gripper_open()
