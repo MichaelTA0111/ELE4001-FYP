43c21c854a3b2f9960d6829d9b6116b7c23148dd
MichaelTA <michaelta2000@protonmail.com>
2023-11-04 20:54:04 +0000

    Update frame rate of camera to 5fps, get midpoints of bounding boxes surrounding coloured blocks

--- src/mirobot_detect.py
+++ src/mirobot_detect.py
@@ -7,7 +7,7 @@ import cv2
 from wlkata_mirobot import WlkataMirobot
 
 from config import MIROBOT_PORT, RESOLUTION_HEIGHT, RESOLUTION_WIDTH
-from opencv_helper_functions import empty, stack_images, draw_contours
+from opencv_helper_functions import stack_images, get_contours
 
 
 x, y, z = 100, 100, 100
@@ -20,42 +20,53 @@ run = True
 def camera_thread():
     global x, y, z, do_open, do_close, run
 
-    cv2.namedWindow('Trackbars')
-    cv2.resizeWindow('Trackbars', 640, 240)
-    cv2.createTrackbar('Hue Min', 'Trackbars', 0, 179, empty)  # Ranges from 0 to 360 or 0 to 180?
-    cv2.createTrackbar('Hue Max', 'Trackbars', 19, 179, empty)
-    cv2.createTrackbar('Sat Min', 'Trackbars', 110, 255, empty)
-    cv2.createTrackbar('Sat Max', 'Trackbars', 240, 255, empty)
-    cv2.createTrackbar('Val Min', 'Trackbars', 153, 255, empty)
-    cv2.createTrackbar('Val Max', 'Trackbars', 255, 255, empty)
-
+    # Set up camera feed
     cap = cv2.VideoCapture(0)
     cap.set(3, RESOLUTION_WIDTH)
     cap.set(4, RESOLUTION_HEIGHT)
+    cap.set(cv2.CAP_PROP_FPS, 5)
+
+    # # Create the trackbar window for HSV values
+    # cv2.namedWindow('Trackbars')
+    # cv2.resizeWindow('Trackbars', 640, 240)
+    # cv2.createTrackbar('Hue Min', 'Trackbars', 100, 179, empty)  # Ranges from 0 to 360 or 0 to 180?
+    # cv2.createTrackbar('Hue Max', 'Trackbars', 112, 179, empty)
+    # cv2.createTrackbar('Sat Min', 'Trackbars', 114, 255, empty)
+    # cv2.createTrackbar('Sat Max', 'Trackbars', 255, 255, empty)
+    # cv2.createTrackbar('Val Min', 'Trackbars', 65, 255, empty)
+    # cv2.createTrackbar('Val Max', 'Trackbars', 206, 255, empty)
+
+    # Set the HSV values
+    blue_hsv_min = np.array([100, 114, 65])
+    blue_hsv_max = np.array([112, 255, 206])
+
+    pts1 = np.float32([[370, 274], [994, 236], [182, 708], [1244, 706]])  # Points on original image
+    pts2 = np.float32([[0, 0], [RESOLUTION_WIDTH, 0],
+                       [0, RESOLUTION_HEIGHT], [RESOLUTION_WIDTH, RESOLUTION_HEIGHT]])  # New output points
+    ct_matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
     while run:
         _, img = cap.read()
-        # img = cv2.flip(img, 1)
-        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        img_warped = cv2.warpPerspective(img, ct_matrix, (RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
+        img_hsv = cv2.cvtColor(img_warped, cv2.COLOR_BGR2HSV)
 
-        hue_min = cv2.getTrackbarPos('Hue Min', 'Trackbars')
-        hue_max = cv2.getTrackbarPos('Hue Max', 'Trackbars')
-        sat_min = cv2.getTrackbarPos('Sat Min', 'Trackbars')
-        sat_max = cv2.getTrackbarPos('Sat Max', 'Trackbars')
-        val_min = cv2.getTrackbarPos('Val Min', 'Trackbars')
-        val_max = cv2.getTrackbarPos('Val Max', 'Trackbars')
-        # print(f'{hue_min} {hue_max} {sat_min} {sat_max} {val_min} {val_max}')
+        # hue_min = cv2.getTrackbarPos('Hue Min', 'Trackbars')
+        # hue_max = cv2.getTrackbarPos('Hue Max', 'Trackbars')
+        # sat_min = cv2.getTrackbarPos('Sat Min', 'Trackbars')
+        # sat_max = cv2.getTrackbarPos('Sat Max', 'Trackbars')
+        # val_min = cv2.getTrackbarPos('Val Min', 'Trackbars')
+        # val_max = cv2.getTrackbarPos('Val Max', 'Trackbars')
 
-        lower = np.array([hue_min, sat_min, val_min])
-        upper = np.array([hue_max, sat_max, val_max])
-        mask = cv2.inRange(img_hsv, lower, upper)
-        img_resultant = cv2.bitwise_and(img, img, mask=mask)
+        blue_mask = cv2.inRange(img_hsv, blue_hsv_min, blue_hsv_max)
+        img_resultant = cv2.bitwise_and(img_warped, img_warped, mask=blue_mask)
         img_canny = cv2.Canny(img_resultant, 50, 50)
-        img_contour = img.copy()
+        img_contour = img_warped.copy()
 
-        draw_contours(img_canny, img_contour)
+        contours = get_contours(img_canny, img_contour)
+        midpoints = [[c[0] + c[2] // 2, c[1] + c[3] // 2] for c in contours]
 
-        img_stack = stack_images(0.5, [[img, img_hsv, mask], [img_resultant, img_canny, img_contour]])
+        img_stack = stack_images(0.5,
+                                 [[img_warped, img_hsv, blue_mask], [img_resultant, img_canny, img_contour]])
 
         cv2.imshow('Image Stack', img_stack)
 
