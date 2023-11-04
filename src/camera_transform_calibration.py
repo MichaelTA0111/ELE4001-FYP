1e9dd45942d4ffc4cec168339127e658254384c5
MichaelTA <michaelta2000@protonmail.com>
2023-11-04 20:51:19 +0000

    Add script to aid in manual calibration of camera transformations

---
+++ src/camera_transform_calibration.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+from opencv_helper_functions import stack_images
+from config import RESOLUTION_HEIGHT, RESOLUTION_WIDTH
+
+
+def detect():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, RESOLUTION_WIDTH)
+    cap.set(4, RESOLUTION_HEIGHT)
+    cap.set(cv2.CAP_PROP_FPS, 5)
+    # fps = int(cap.get(5))
+    # print("fps:", fps)
+
+    while True:
+        _, img = cap.read()
+
+        pts1 = np.float32([[370, 274], [994, 236], [182, 708], [1244, 706]])  # Points on original image
+        pts2 = np.float32([[0, 0], [RESOLUTION_WIDTH, 0],
+                           [0, RESOLUTION_HEIGHT], [RESOLUTION_WIDTH, RESOLUTION_HEIGHT]])  # New output points
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+        img_warped = cv2.warpPerspective(img, matrix, (RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
+
+        img_stack = stack_images(0.5, [[img], [img_warped]])
+
+        cv2.imshow('Image Stack', img_stack)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+
+if __name__ == '__main__':
+    detect()
